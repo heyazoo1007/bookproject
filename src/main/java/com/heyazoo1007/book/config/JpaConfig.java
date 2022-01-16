5a5e0c49c52033f14881fa435f09c1bec14ea68b
heyazoo1007 <heyazoo1007@gmail.com>
2022-01-16 15:43:14 +0900

    구글,네이버 로그인 기능 구현

---
+++ src/main/java/com/heyazoo1007/book/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.heyazoo1007.book.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
