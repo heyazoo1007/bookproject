5e6ccef81815c399097ed66c721ede7bb9a1096f
heyazoo1007 <heyazoo1007@gmail.com>
2022-01-22 21:16:10 +0900

    /bookshelf 404 문제 해결시도1

--- src/main/java/com/heyazoo1007/book/Application.java
+++ src/main/java/com/heyazoo1007/book/Application.java
@@ -2,10 +2,12 @@ package com.heyazoo1007.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"web"})
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
